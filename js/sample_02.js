270eec6501d415a212c7e169a8d7e74970170265
y_tsubasa <tbs_chako@yahoo.co.jp>
2019-11-14 22:10:38 +0900

    「続きを読む」の実装を改良する(不要な変数宣言を削除する)

--- js/sample_02.js
+++ js/sample_02.js
@@ -1,7 +1,5 @@
-var text = '';
-
 $(function(){
-  text = $("#article").text();
+  var text = $("#article").text();
   var text_part = text.substr(0, 140);
   var text_gray = text.substr(140, 10) + '...';
   $("#article").text(text_part);
